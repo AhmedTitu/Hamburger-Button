ae2fd9a241071e296ce12093c06729b1a6a7e7df
AhmedTitu <ahmedibnahasan@gmail.com>
2021-06-10 12:11:19 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#btn')
+const nav = document.querySelector('#nav')
+
+
+btn.addEventListener('click' , () => {
+    nav.classList.toggle('active')
+    btn.classList.toggle('active')
+})
